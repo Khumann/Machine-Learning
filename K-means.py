798cb3f59fed0588b21d886d5d32fd5f2f991685
Shubham <chaudhary.shubham812@gmail.com>
2024-05-13 23:05:39 +0530

    created k-means.py

---
+++ K-means.py
@@ -0,0 +1,7 @@
+import numpy as np
+from sklearn import datasets
+from sklearn import datasets
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split
+from scipy.stats import norm
+import matplotlib.pyplot as plt
