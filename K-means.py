979bc61743ceeb5c8cb875380ff3cd0e3f418e26
Shubham <chaudhary.shubham812@gmail.com>
2024-05-14 22:05:04 +0100

    deleted file

--- K-means.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-from sklearn import datasets
-from sklearn import datasets
-from sklearn.model_selection import train_test_split
-from sklearn.model_selection import train_test_split
-from scipy.stats import norm
-import matplotlib.pyplot as plt
